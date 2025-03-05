9cac113a203a3b4616ba2695dbe031bf4cf72cb7
sentsuetrong <sentsuetrong@gmail.com>
2025-03-05 07:59:25 +0700

    trying to debug a system

--- codeigniter/app/Controllers/Home.php
+++ codeigniter/app/Controllers/Home.php
@@ -6,6 +6,23 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        $uri = $this->request->getUri();
+        $path = $uri->getPath();
+
+        $data = [
+            'uri' => $uri,
+            'path' => $path,
+            'isAdminPage' => preg_match('/\/admin/i', $path)
+        ];
+
+        return view('welcome_message', $data);
+    }
+
+    public function test()
+    {
+        $routes = \Config\Services::routes();
+        echo '<pre>';
+        var_dump($routes->getRoutes());
+        echo '</pre>';
     }
 }
